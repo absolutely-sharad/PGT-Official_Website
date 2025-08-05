96468f3a26fc64e1a0055446635e36c3aaf574bb
Sharad Singh Kushwaha <153629616+absolutely-sharad@users.noreply.github.com>
2025-08-05 14:22:58 +0530

    Updated Blog.tsx

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,1 +1,12 @@
-{"code":"rate-limited","message":"You have hit the rate limit. Please upgrade to keep chatting.","providerLimitHit":false,"isRetryable":true}
+import React from "react";
+
+const Blog = () => {
+  return (
+    <section>
+      <h1>Welcome to the Blog</h1>
+      {/* Add blog content here */}
+    </section>
+  );
+};
+
+export default Blog;
